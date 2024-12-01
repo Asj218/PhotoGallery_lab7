12bc1256988be5d92772d175b8a2339bf2301418
Анастасия <azizovaa031@gmail.com>
2024-12-02 01:19:30 +0700

    Добавление зависимости lifecycle-extensions (app/build.gradle)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
     //implementation("androidx.recyclerview:recyclerview:1.0.0")//или
+    implementation("androidx.lifecycle:lifecycle-extensions:2.0.0")
     implementation("com.google.code.gson:gson:2.8.5")
     implementation("com.squareup.retrofit2:converter-gson:2.4.0")
     implementation("com.squareup.retrofit2:retrofit:2.5.0")
