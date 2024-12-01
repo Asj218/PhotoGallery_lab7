ea78aa01392a83f824acdb4034f9880ff9a74dc3
Анастасия <azizovaa031@gmail.com>
2024-12-02 00:25:08 +0700

    Получение объекта Call, выполняющего запрос (PhotoGalleryFragment.kt)

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.photogallery.api.FlickrApi
+import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 
@@ -22,6 +23,7 @@ class PhotoGalleryFragment : Fragment() {
                 .addConverterFactory(ScalarsConverterFactory.create())
                 .build()
         val flickrApi: FlickrApi = retrofit.create(FlickrApi::class.java)
+        val flickrHomePageRequest: Call<String> = flickrApi.fetchContents()
     }
 
     override fun onCreateView(
