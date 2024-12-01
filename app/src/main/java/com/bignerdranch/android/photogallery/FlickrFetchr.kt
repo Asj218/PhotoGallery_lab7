ff38aacce9472a4ebcb8df4ddad97e38a4459816
Анастасия <azizovaa031@gmail.com>
2024-12-02 00:34:45 +0700

    Создание FlickrFetchr (FlickrFetchr.kt)

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.kt
@@ -0,0 +1,20 @@
+package com.bignerdranch.android.photogallery
+
+import com.bignerdranch.android.photogallery.api.FlickrApi
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+private const val TAG = "FlickrFetchr"
+
+class FlickrFetchr {
+    private val flickrApi: FlickrApi
+
+    init {
+        val retrofit: Retrofit = Retrofit.Builder()
+                .baseUrl("https://www.flickr.com/")
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .build()
+
+        flickrApi = retrofit.create(FlickrApi::class.java)
+    }
+}
