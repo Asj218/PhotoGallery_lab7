e2d4b4a4237211a7754b8cc23c62c42a968c1bf4
Анастасия <azizovaa031@gmail.com>
2024-12-02 01:25:26 +0700

    Получение экземпляра ViewModel от провайдера (PhotoGalleryFragment.kt)

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.photogallery.api.FlickrApi
@@ -20,17 +21,13 @@ import retrofit2.converter.scalars.ScalarsConverterFactory
 private const val TAG = "PhotoGalleryFragment"
 
 class PhotoGalleryFragment : Fragment() {
+    private lateinit var photoGalleryViewModel: PhotoGalleryViewModel
     private lateinit var photoRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val flickrLiveData: LiveData<List<GalleryItem>> = FlickrFetchr().fetchPhotos()
-        flickrLiveData.observe(
-            this,
-            Observer { galleryItems ->
-                Log.d(TAG, "Response received: $galleryItems")
-            })
+        photoGalleryViewModel = ViewModelProviders.of(this).get (PhotoGalleryViewModel::class.java)
     }
 
     override fun onCreateView(
