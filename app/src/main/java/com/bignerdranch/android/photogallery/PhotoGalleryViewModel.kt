6e52083f151aee486b4515461b34e38350ffd57a
Анастасия <azizovaa031@gmail.com>
2024-12-13 21:38:36 +0700

    Добавление класса для работы с хранимым запросом (QueryPreferences.kt)

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryViewModel.kt
@@ -2,19 +2,21 @@ package com.bignerdranch.android.photogallery
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-//import androidx.lifecycle.Transformations
+//import androidx.lifecycle.Transformations // Убедитесь, что этот импорт присутствует
 import androidx.lifecycle.ViewModel
 
 class PhotoGalleryViewModel : ViewModel() {
     val galleryItemLiveData: LiveData<List<GalleryItem>>
     private val flickrFetchr = FlickrFetchr()
     private val mutableSearchTerm = MutableLiveData<String>()
+
     init {
         mutableSearchTerm.value = "planets"
-        galleryItemLiveData =  Transformations.switchMap(mutableSearchTerm) {
-            searchTerm -> flickrFetchr.searchPhotos(searchTerm)
+        galleryItemLiveData = Transformations.switchMap(mutableSearchTerm) { searchTerm: String -> // Указан тип параметра
+            flickrFetchr.searchPhotos(searchTerm)
         }
     }
+
     fun fetchPhotos(query: String = "") {
         mutableSearchTerm.value = query
     }
