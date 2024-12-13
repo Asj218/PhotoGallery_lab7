d5a9f11a2b3f5d5a352abf1f5f8e8c3d0b8717f2
Анастасия <azizovaa031@gmail.com>
2024-12-13 21:51:34 +0700

    Добавление зависимости core-ktx (app/build.gradle)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     //implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")//
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.5.1")//
+    implementation("androidx.core:corektx:1.0.0")
     implementation("com.google.code.gson:gson:2.8.5")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
