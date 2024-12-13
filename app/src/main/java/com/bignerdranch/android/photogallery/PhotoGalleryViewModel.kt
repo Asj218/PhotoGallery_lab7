aecd6801c9fafaf05f76820d04246bef74eccc19
Анастасия <azizovaa031@gmail.com>
2024-12-13 21:44:37 +0700

    Очистка сохраненного запроса (PhotoGalleryFragment.kt)

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryViewModel.kt
@@ -1,23 +1,26 @@
 package com.bignerdranch.android.photogallery
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 //import androidx.lifecycle.Transformations // Убедитесь, что этот импорт присутствует
 import androidx.lifecycle.ViewModel
 
-class PhotoGalleryViewModel : ViewModel() {
+class PhotoGalleryViewModel(private val app: Application) : AndroidViewModel(app) {
     val galleryItemLiveData: LiveData<List<GalleryItem>>
     private val flickrFetchr = FlickrFetchr()
     private val mutableSearchTerm = MutableLiveData<String>()
 
     init {
-        mutableSearchTerm.value = "planets"
+        mutableSearchTerm.value = QueryPreferences.getStoredQuery(app)
         galleryItemLiveData = Transformations.switchMap(mutableSearchTerm) { searchTerm: String -> // Указан тип параметра
             flickrFetchr.searchPhotos(searchTerm)
         }
     }
 
     fun fetchPhotos(query: String = "") {
+        QueryPreferences.setStoredQuery(app, query)
         mutableSearchTerm.value = query
     }
 }
