cbb3520cebf87d3d4f731f6dcbb4fd462064a593
Анастасия <azizovaa031@gmail.com>
2024-12-13 20:36:07 +0700

    Добавление XML-файла меню (res/menu/fragment_photo_gallery.xml)

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryViewModel.kt
@@ -7,6 +7,12 @@ class PhotoGalleryViewModel : ViewModel() {
     val galleryItemLiveData: LiveData<List<GalleryItem>>
     init {
         galleryItemLiveData = FlickrFetchr().searchPhotos("planets")
-
+//897 Создайте
+//XML-файл
+//res/menu/fragment_photo_gallery.xml
+//меню
+// для
+// PhotoGallery Fragment (если забыли, как что делается,
+// вернитесь к главе 14).
     }
 }
