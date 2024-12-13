33b400dde32e98832acecda7ba234b88c825c786
Анастасия <azizovaa031@gmail.com>
2024-12-13 20:57:04 +0700

    Хранение последнего запроса в PhotoGalleryViewModel (PhotoGalleryViewModel.kt)

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryViewModel.kt
@@ -1,18 +1,21 @@
 package com.bignerdranch.android.photogallery
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+//import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class PhotoGalleryViewModel : ViewModel() {
     val galleryItemLiveData: LiveData<List<GalleryItem>>
+    private val flickrFetchr = FlickrFetchr()
+    private val mutableSearchTerm = MutableLiveData<String>()
     init {
-        galleryItemLiveData = FlickrFetchr().searchPhotos("planets")
-//897 Создайте
-//XML-файл
-//res/menu/fragment_photo_gallery.xml
-//меню
-// для
-// PhotoGallery Fragment (если забыли, как что делается,
-// вернитесь к главе 14).
+        mutableSearchTerm.value = "planets"
+        galleryItemLiveData =  Transformations.switchMap(mutableSearchTerm) {
+            searchTerm -> flickrFetchr.searchPhotos(searchTerm)
+        }
+    }
+    fun fetchPhotos(query: String = "") {
+        mutableSearchTerm.value = query
     }
 }
